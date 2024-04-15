03e4e89011721c0dfaaef2534a4bb60c17e1fb3d
Yiqi Wang <nobibi23@gmail.com>
2024-04-14 19:18:03 -0600

    1
    1

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://nobibi:qwert12345@cluster0.lr0rnwi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = "mongodb+srv://nobibi:barbala9197@cluster0.lr0rnwi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -16,7 +16,7 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
+        await client.db("password").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
@@ -24,3 +24,12 @@ async function run() {
     }
 }
 run().catch(console.dir);
+
+
+
+
+
+
+
+
+
